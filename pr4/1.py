86e1fb1ce313b3006623ea3302b3a296454c9ce1
Guk1qq <anyamaksimenkoa@gmail.com>
2024-10-04 20:29:00 +0300

    1.py

--- pr4/1.py
+++ pr4/1.py
@@ -1,5 +1,5 @@
-a = int(input('Введите число a: ')
-b = int(input('Введите число b: ')
+a = int(input('Введите число a: '))
+b = int(input('Введите число b: '))
 while a <= b:
     print(a)
     a += 1
