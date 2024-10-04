b6180b29f2b3fc5bf2dafb618f0ce64af3a738ab
Guk1qq <anyamaksimenkoa@gmail.com>
2024-10-04 19:22:11 +0300

    2.py

--- pr4/2.py
+++ pr4/2.py
@@ -1,1 +1,10 @@
-
+a = int(input('Введите число a: ')
+b = int(input('Введите число b: ')
+if a < b:
+      while a <= b:
+          print(a)
+          a += 1
+else:
+      while a >= b:
+          print(a)
+          a -= 1
