d3dfe2ca751e9d3d3d48c70138233c7992413fcc
Guk1qq <anyamaksimenkoa@gmail.com>
2024-11-27 16:45:08 +0300

    Update programm.py

--- pr12/programm.py
+++ pr12/programm.py
@@ -13,15 +13,14 @@ c1 = json.dumps(user_data1)
 print('-'*50)
 pprint(c1)
 user_data = json.loads(c1)
-print('-'*50)
 pprint(user_data)
 info = {
-        'company'   : (user_data["company"]),
+        'company': (user_data["company"]),
         'created_at': (user_data["created_at"]),
-        'email'     : (user_data["email"]),
-        'id'        : (user_data["id"]),
-        'name'      : (user_data["name"]),
-        'url'       : (user_data["url"])
+        'email': (user_data["email"]),
+        'id': (user_data["id"]),
+        'name': (user_data["name"]),
+        'url': (user_data["url"])
         }
 pprint(info)
 print('-'*50)
